772f81b902f7595b382bf69b9843fb14ce432db1
Adeyemi <adeyemiezekiel26@gmail.com>
2024-02-28 15:33:29 -0800

    testing

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,9 +8,9 @@ export const Skills = ()=>{
 Aos.init()
   },[])
     return( 
-<>
-    <div id="Skills" data-aos="fade-up"
-    data-aos-duration="2000" className="grid grid-flow-row border-t mt-[8rem] pt-12 lg:pt-20 sm:grid-cols-3 mx-4 md:mx-6 lg:mx-28 justify-center text-center md:text-left md:gap-x-14 gap-10 md:gap-6 lg:gap-10 ">
+<section data-aos="fade-up"
+    data-aos-duration="2000" >
+    <div id="Skills" className="grid grid-flow-row border-t mt-[8rem] pt-12 lg:pt-20 sm:grid-cols-3 mx-4 md:mx-6 lg:mx-28 justify-center text-center md:text-left md:gap-x-14 gap-10 md:gap-6 lg:gap-10 ">
    <div>
     <h2>HTML</h2>
     <span>3 Years of Experience</span>
@@ -39,15 +39,13 @@ Aos.init()
      <div>
      <h2>Tailwind</h2> 
      <span>3 Years of Experience</span>
-     </div>
+     </div> 
 </div>
 
-
-     <div className="flex justify-end ...">
+<div className="flex justify-end ...">
   <img className='pb-10' src={leftnet} alt="" /> 
      </div>
 
-
-     </>
+     </section>
     )
 }
