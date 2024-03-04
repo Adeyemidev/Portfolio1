23a289d4e2a40def1112ab3981f7f5f64cef808a
Adeyemi <adeyemiezekiel26@gmail.com>
2024-03-04 07:17:00 -0800

    link changed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -80,7 +80,7 @@ I would love to hear about your project and how I could help.
 
  <a href="https://github.com/Adeyemidev" > <img src={github} alt="" /></a>
  <img src={icon} alt=""/>
- <a href="www.linkedin.com/in/adeyemi-ezekiel" > <img src={linkedIn} alt="" /></a>
+ <a href="https://linkedin.com/in/adeyemi-ezekiel" > <img src={linkedIn} alt="" /></a>
  <a href="https://x.com/easycode01?t=XbtFtZ4qzl97gErjSosZsQ&s=09" > <img src={twitter} alt="" /></a>
 </ul>
 </nav>
